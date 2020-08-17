48cc402f11085a3354353c682fb535b27abd1710
Rahul Kapoor <rahul253801@gmail.com>
2020-08-17 20:44:01 +0530

    added user secrets key for JWT token

--- Infrastructure/Security/JWTGenerator.cs
+++ Infrastructure/Security/JWTGenerator.cs
@@ -4,20 +4,26 @@ using System.Security.Claims;
 using System.Text;
 using Application.Interfaces;
 using Domain;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
     public class JWTGenerator : IJWTGenerator
     {
+        private readonly SymmetricSecurityKey _key;
+        public JWTGenerator(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser appUser)
         {
             var claims = new List<Claim>() {
                 new Claim(JwtRegisteredClaimNames.NameId, appUser.UserName)
             };
             //security key on server
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
             //defining token details
             var tokenDescriptor = new SecurityTokenDescriptor()
