6766b0c1eb2763593bb532134c34380da2fde4e5
Rahul Kapoor <rahul253801@gmail.com>
2020-08-17 20:54:07 +0530

    added authorize filter to every api call except login

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using Application.Users;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
         // POST api/user
--- API/Startup.cs
+++ API/Startup.cs
@@ -7,9 +7,11 @@ using FluentValidation.AspNetCore;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -44,7 +46,12 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddMvc().AddFluentValidation(cfg =>
+            services.AddControllers(opt =>
+            {
+                //configure to authorize bearer token with every API call
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            }).AddFluentValidation(cfg =>
             {
                 cfg.RegisterValidatorsFromAssemblyContaining<Create>();
             });
