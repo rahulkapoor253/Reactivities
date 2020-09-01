80d00dcb5b51778ba92644dab48d2467969bb574
Rahul Kapoor <rahul253801@gmail.com>
2020-09-01 22:47:53 +0530

    added checks on photo tab for upload, main feature

--- client-app/src/features/profile/ProfilePhotos.tsx
+++ client-app/src/features/profile/ProfilePhotos.tsx
@@ -1,23 +1,42 @@
-import React, { useContext } from "react";
-import { Tab, Header, Card, Image } from "semantic-ui-react";
+import React, { useContext, useState } from "react";
+import { Tab, Header, Card, Image, Button } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { RootStoreContext } from "../../app/stores/rootStore";
 
 const ProfilePhotos = () => {
   const rootStore = useContext(RootStoreContext);
-  const { profile } = rootStore.profileStore;
+  const { profile, isCurrentUser } = rootStore.profileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
 
   return (
     <Tab.Pane>
-      <Header content="Photos" icon="image" />
-      <Card.Group itemsPerRow={5}>
-        {profile &&
-          profile.photos.map((photo) => (
-            <Card key={photo.id}>
-              <Image src={photo.url} />
-            </Card>
-          ))}
-      </Card.Group>
+      <Header floated="left" content="Photos" icon="image" />
+      {isCurrentUser && (
+        <Button
+          floated="right"
+          content={addPhotoMode ? "Cancel" : "Add photo"}
+          basic
+          onClick={() => setAddPhotoMode(!addPhotoMode)}
+        />
+      )}
+      {addPhotoMode ? (
+        <p>My photo widget</p>
+      ) : (
+        <Card.Group itemsPerRow={5}>
+          {profile &&
+            profile.photos.map((photo) => (
+              <Card key={photo.id}>
+                <Image src={photo.url} />
+                {isCurrentUser && (
+                  <Button.Group fluid widths="2">
+                    <Button basic positive content="Main" />
+                    <Button basic negative icon="trash" />
+                  </Button.Group>
+                )}
+              </Card>
+            ))}
+        </Card.Group>
+      )}
     </Tab.Pane>
   );
 };
